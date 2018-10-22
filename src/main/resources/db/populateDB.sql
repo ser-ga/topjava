291fdb65e508b67be4deedd46098f6e0b5b18d29
Zhukov Sergey <1083511@gmail.com>
2018-10-23 01:24:09 +0300

    HW3

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -9,3 +9,7 @@ INSERT INTO users (name, email, password) VALUES
 INSERT INTO user_roles (role, user_id) VALUES
   ('ROLE_USER', 100000),
   ('ROLE_ADMIN', 100001);
+
+INSERT INTO meals (id, date_time, description, calories, user_id) VALUES
+  (100000, '2018-10-22 12:00:00', 'Админ ланч', 510, 100001),
+  (100001, '2018-10-22 19:00:00', 'Админ ужин', 1510, 100001);
