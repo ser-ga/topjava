69dc8b7f981ad0dd9e1771b9b0064fcba4134f0f
Zhukov Sergey <1083511@gmail.com>
2018-11-21 22:36:05 +0300

    HW6 2.4 ResultSetExtractor

--- src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcUserRepositoryImpl.java
+++ src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcUserRepositoryImpl.java
@@ -1,10 +1,11 @@
 package ru.javawebinar.topjava.repository.jdbc;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.dao.support.DataAccessUtils;
 import org.springframework.jdbc.core.BatchPreparedStatementSetter;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.ResultSetExtractor;
 import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
@@ -15,23 +16,39 @@ import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.UserRepository;
 
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
-import java.util.StringTokenizer;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Repository
 @Transactional(readOnly = true)
 public class JdbcUserRepositoryImpl implements UserRepository {
 
-    private static final RowMapper<User> USER_WITH_ROLES_MAPPER = (rs, rowNum) -> {
-        List<Role> roles = new ArrayList<>();
-        StringTokenizer st = new StringTokenizer(rs.getString("roles").replace("ARRAY", ""), "{,}'[]");
-        while (st.hasMoreTokens()) {
-            roles.add(Role.valueOf(st.nextToken()));
+    private static final ResultSetExtractor<List<User>> USER_WITH_ROLES_RS_EXTRACTOR = new ResultSetExtractor<>() {
+        @Override
+        public List<User> extractData(ResultSet rs) throws SQLException, DataAccessException {
+            List<Integer> orderList = new ArrayList<>();
+            Map<Integer, User> users = new HashMap<>();
+            while (rs.next()) {
+                int user_id = rs.getInt("id");
+                if (users.containsKey(user_id)) {
+                    List<Role> roles = new ArrayList<>(users.get(user_id).getRoles());
+                    roles.add(Role.valueOf(rs.getString("role")));
+                    users.get(user_id).setRoles(roles);
+                } else {
+                    users.put(user_id, new User(user_id, rs.getString("name"), rs.getString("email"),
+                            rs.getString("password"), rs.getInt("calories_per_day"),
+                            rs.getBoolean("enabled"), rs.getDate("registered"),
+                            List.of(Role.valueOf(rs.getString("role")))));
+                    orderList.add(user_id);
+                }
+            }
+            return orderList.stream().map(users::get).collect(Collectors.toList());
         }
-        return new User(rs.getInt("id"), rs.getString("name"), rs.getString("email"),
-                rs.getString("password"), rs.getInt("calories_per_day"), rs.getBoolean("enabled"), rs.getDate("registered"), roles);
     };
 
     private final JdbcTemplate jdbcTemplate;
@@ -76,27 +93,27 @@ public class JdbcUserRepositoryImpl implements UserRepository {
 
     @Override
     public User get(int id) {
-        List<User> users = jdbcTemplate.query("SELECT u.*, ARRAY_AGG(r1.role) AS roles FROM users u " +
+        List<User> users = jdbcTemplate.query("SELECT u.*, r1.role FROM users u " +
                 "LEFT JOIN user_roles r1 ON u.id = r1.user_id " +
-                "WHERE u.id = ? GROUP BY u.id", USER_WITH_ROLES_MAPPER, id);
+                "WHERE u.id = ?", USER_WITH_ROLES_RS_EXTRACTOR, id);
         return DataAccessUtils.singleResult(users);
     }
 
     @Override
     public User getByEmail(String email) {
 //        return jdbcTemplate.queryForObject("SELECT * FROM users WHERE email=?", ROW_MAPPER, email);
-        List<User> users = jdbcTemplate.query("SELECT u.*, ARRAY_AGG(r1.role) AS roles FROM users u " +
+        List<User> users = jdbcTemplate.query("SELECT u.*, r1.role FROM users u " +
                 "LEFT JOIN user_roles r1 ON u.id = r1.user_id " +
-                "WHERE u.email = ? GROUP BY u.id", USER_WITH_ROLES_MAPPER, email);
+                "WHERE u.email = ?", USER_WITH_ROLES_RS_EXTRACTOR, email);
         return DataAccessUtils.singleResult(users);
     }
 
     @Override
     public List<User> getAll() {
 //        return jdbcTemplate.query("SELECT * FROM users ORDER BY name, email", ROW_MAPPER);
-        return jdbcTemplate.query("SELECT u.*, ARRAY_AGG(r1.role) AS roles FROM users u " +
+        return jdbcTemplate.query("SELECT u.*, r1.role FROM users u " +
                 "LEFT JOIN user_roles r1 ON u.id = r1.user_id " +
-                "GROUP BY u.id ORDER BY u.name, u.email", USER_WITH_ROLES_MAPPER);
+                "ORDER BY u.name, u.email", USER_WITH_ROLES_RS_EXTRACTOR);
     }
 
     private void insertRoles(final List<Role> roles, Integer user_id) {
